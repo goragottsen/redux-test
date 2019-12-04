5f48d84ca25ebaa530363a790c54df3236d4c283
anaroldugina <ana@property.ca>
2019-12-03 21:27:07 -0500

    Completed react part

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts'
+import PostForm from './components/PostForm'
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <PostForm />
       <Posts />
     </div>
   );
